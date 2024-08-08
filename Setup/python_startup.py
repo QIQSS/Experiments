770555fbf5197ea14247a48a8f893e840ffc609e
Alexis Morel <mora0502@usherbrooke.ca>
2024-08-08 11:15:16 -0400

    update

--- Setup/python_startup.py
+++ Setup/python_startup.py
@@ -2,16 +2,22 @@
 my ipython startup config
 """
 
-# iPython:
+# ipython:
+from IPython import get_ipython
 if (ipy := get_ipython()):
     ipy.run_line_magic('load_ext', 'autoreload')
     ipy.run_line_magic('autoreload', '2')
     ipy.run_line_magic('autocall', '1')
+    ipy.run_line_magic('gui', 'qt')
 
 # Packages:
 import numpy as np
 from matplotlib import pyplot as plt
 
 # PyHegel:
-from pyHegel.scipy_fortran_fix import fix_problem_new
-from pyHegel.commands import *
+try:
+    from pyHegel.scipy_fortran_fix import fix_problem_new
+    from pyHegel.commands import *
+    _init_pyHegel_globals()
+except ImportError:
+    print('no pyHegel.')
